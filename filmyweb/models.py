598c3c6c3f9e2dec44d044f1109a05e28c94a9b2
Radwan16 <32171764+Radwan16@users.noreply.github.com>
2023-05-04 21:35:26 +0200

    Create more models

--- filmyweb/models.py
+++ filmyweb/models.py
@@ -3,6 +3,13 @@ from django.db import models
 class Film(models.Model):
     tytul= models.CharField(max_length=60,  blank= False ,unique=True)
     rok = models.PositiveSmallIntegerField(default=2000)
+    opis = models.TextField(default="")
+    premiera = models.DateField(null=True, blank=True)
+    imdb_rating=models.DecimalField(max_digits= 4, decimal_places= 2, null=True, blank=True)
+    plakat= models.ImageField(upload_to="plakaty", null=True, blank= True)
 
     def __str__(self):
         return self.tytul + '(' + str(self.rok) + ')'
+
+    def tytul_z_rokiem(self):
+        return "{} ({})".format(self.tytul, self.rok)
