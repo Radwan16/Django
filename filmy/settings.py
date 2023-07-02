af4ca1ce423fb96d3f6e48a0b5be324e7f4f9e07
Radwan16 <32171764+Radwan16@users.noreply.github.com>
2023-07-02 22:32:34 +0200

    Media files

--- filmy/settings.py
+++ filmy/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['moje_static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT ='moje_media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- filmyweb/models.py
+++ filmyweb/models.py
@@ -6,7 +6,8 @@ class Film(models.Model):
     opis = models.TextField(default="")
     premiera = models.DateField(null=True, blank=True)
     imdb_rating=models.DecimalField(max_digits= 4, decimal_places= 2, null=True, blank=True)
-    plakat= models.ImageField(upload_to="plakaty", null=True, blank= True)
+
+    plakat = models.ImageField(upload_to="plakaty", null=True, blank= True)
 
     def __str__(self):
         return self.tytul + '(' + str(self.rok) + ')'
