bd1f85ec9c4799b51a9211aafa59d46fa2277922
Radwan16 <32171764+Radwan16@users.noreply.github.com>
2023-07-09 14:37:42 +0200

    CREATE - new movie

--- filmyweb/urls.py
+++ filmyweb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from filmyweb.views import wszystkie_filmy
+from filmyweb.views import wszystkie_filmy,nowy_film
 
 urlpatterns = [
-    path('test/',wszystkie_filmy)
+    path('wszystkie/',wszystkie_filmy),
+    path('nowy/',nowy_film),
 ]
--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Film
+from .forms import FilmForm
 def wszystkie_filmy(request):
     wszystkie=  Film.objects.all()
     return render(request, 'filmy.html',{'filmy': wszystkie})
+def nowy_film(request):
+    form =FilmForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'nowy_film.html',{'form':form})
