c3e6328a57106bc1278a0dc4bbfe5a82483f9b12
Radwan16 <32171764+Radwan16@users.noreply.github.com>
2023-08-09 19:57:05 +0200

    Repair Add button

--- filmyweb/forms.py
+++ filmyweb/forms.py
@@ -2,6 +2,6 @@ from django.forms import ModelForm
 from .models import Film
 
 class FilmForm(ModelForm):
-    class Meta():
+    class Meta:
         model = Film
         fields = ['tytul','rok','premiera','opis','imdb_rating','plakat']
--- filmyweb/views.py
+++ filmyweb/views.py
@@ -9,11 +9,12 @@ def wszystkie_filmy(request):
 @login_required
 def nowy_film(request):
     form = FilmForm(request.POST or None, request.FILES or None)
+
     if form.is_valid():
         form.save()
         return redirect(wszystkie_filmy)
 
-    return render(request,'film_form.html',{'form':form})
+    return render(request,'nowy_film.html',{'form':form})
 @login_required
 def edytuj_film(request,id):
     film = get_object_or_404(Film,pk=id)
@@ -23,7 +24,7 @@ def edytuj_film(request,id):
         form.save()
         return redirect(wszystkie_filmy)
 
-    return render(request,'film_form.html',{'form':form})
+    return render(request,'nowy_film.html',{'form':form})
 @login_required
 def usun_film(request,id):
     film = get_object_or_404(Film,pk=id)
